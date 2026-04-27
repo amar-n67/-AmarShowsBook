7fb61dfd5e878ccbdf8f1267b5d307f4107b926f
Amar <amarnath4172@gmail.com>
2026-04-27 23:07:04 +0530

    Updated the session out with timer.. User will logout automatically after 5 min if no activity found...

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f46d275102bcb106f6d220fe7f416b1da2094b1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bccf97074c4605772fc7babce6bf4ff3cad04d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
