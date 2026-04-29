db4a86dba1fbe1200bbe5f3fdd010b97c888ce09
Amar <amarnath4172@gmail.com>
2026-04-29 22:02:27 +0530

    added docker support

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da98121daa4d0951bcee427a82cc22f3be5f391a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040555923d1857fbe753979074e09f878c99357d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
