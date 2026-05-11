31fd0a63fc7f8d8334a30a2cbd6982f7e7a8b676
Amar <amarnath4172@gmail.com>
2026-05-11 17:59:42 +0530

    Create RBAC infrastructure files

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c02e788ede17bbb7e4a333acdcf81f483fd472")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fade475a5ad5333679035074fb103f76d912c3f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
