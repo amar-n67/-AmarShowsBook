4457686f2e2d9bd0fe5586291b1b8986a0d2b708
Amar <amarnath4172@gmail.com>
2026-05-11 17:27:30 +0530

    Fix nullable booking timestamp rendering

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2af17c5acd0c98f48cde48c49f9c03e8e0eed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffbc4c7259a8e8a19c9d84dfc671e7b1de743db")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
