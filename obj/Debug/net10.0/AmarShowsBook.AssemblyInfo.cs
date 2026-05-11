2980c300e7e816a4afbe4a29c9b14fdac6a1b364
Amar <amarnath4172@gmail.com>
2026-05-11 19:08:20 +0530

    Add user status fields for admin management

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2296d8e81183bb5e887143787b2f44ce29d3434a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263dbf571a389c2245b2611e59a5129c09760dc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
