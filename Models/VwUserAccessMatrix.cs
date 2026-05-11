d2532d20a5a6753171d57a19dbf6c637992a97b8
Amar <amarnath4172@gmail.com>
2026-05-11 18:21:20 +0530

    Complete enterprise RBAC access control system

--- Models/VwUserAccessMatrix.cs
+++ Models/VwUserAccessMatrix.cs
@@ -1,31 +1,42 @@
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AmarShowsBook.Models
 {
-    // Represents user role permission matrix SQL view
     [Keyless]
     public class VwUserAccessMatrix
     {
+        [Column("user_id")]
         public int UserId { get; set; }
 
-        public string UserName { get; set; }
+        [Column("user_name")]
+        public string? UserName { get; set; }
 
-        public string UserEmail { get; set; }
+        [Column("user_email")]
+        public string? UserEmail { get; set; }
 
-        public string RoleCode { get; set; }
+        [Column("role_code")]
+        public string? RoleCode { get; set; }
 
-        public string RoleName { get; set; }
+        [Column("role_name")]
+        public string? RoleName { get; set; }
 
-        public string ModuleCode { get; set; }
+        [Column("module_code")]
+        public string? ModuleCode { get; set; }
 
-        public string ModuleName { get; set; }
+        [Column("module_name")]
+        public string? ModuleName { get; set; }
 
-        public string PermissionCode { get; set; }
+        [Column("permission_code")]
+        public string? PermissionCode { get; set; }
 
-        public string PermissionName { get; set; }
+        [Column("permission_name")]
+        public string? PermissionName { get; set; }
 
-        public string ActionType { get; set; }
+        [Column("action_type")]
+        public string? ActionType { get; set; }
 
+        [Column("is_active")]
         public bool IsActive { get; set; }
     }
 }
--- Models/VwUserApplicationMenu.cs
+++ Models/VwUserApplicationMenu.cs
@@ -1,37 +1,54 @@
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AmarShowsBook.Models
 {
-    // Represents dynamic user menu access SQL view
+    // SQL VIEW:
+    // vw_user_application_menus
     [Keyless]
     public class VwUserApplicationMenu
     {
+        [Column("user_id")]
         public int UserId { get; set; }
 
-        public string UserName { get; set; }
+        [Column("user_name")]
+        public string? UserName { get; set; }
 
-        public string RoleCode { get; set; }
+        [Column("role_code")]
+        public string? RoleCode { get; set; }
 
+        [Column("menu_id")]
         public int MenuId { get; set; }
 
-        public string MenuCode { get; set; }
+        [Column("menu_code")]
+        public string? MenuCode { get; set; }
 
-        public string MenuName { get; set; }
+        [Column("menu_name")]
+        public string? MenuName { get; set; }
 
-        public string RoutePath { get; set; }
+        [Column("route_path")]
+        public string? RoutePath { get; set; }
 
-        public string IconName { get; set; }
+        [Column("icon_name")]
+        public string? IconName { get; set; }
 
+        [Column("menu_level")]
         public int MenuLevel { get; set; }
 
+        [Column("display_order")]
         public int DisplayOrder { get; set; }
 
+        // PostgreSQL uses snake_case column names
+        [Column("can_view")]
         public bool CanView { get; set; }
 
+        [Column("can_create")]
         public bool CanCreate { get; set; }
 
+        [Column("can_update")]
         public bool CanUpdate { get; set; }
 
+        [Column("can_delete")]
         public bool CanDelete { get; set; }
     }
 }
