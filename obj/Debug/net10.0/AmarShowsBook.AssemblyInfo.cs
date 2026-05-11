6d8630efea283e1a78af2e106d665d1579bcfa93
Amar <amarnath4172@gmail.com>
2026-05-11 22:57:48 +0530

    Checkpoint before enterprise RBAC implementation

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec74271b39eae1b34c8876b9e7e3c085bb37462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516d04061407b4ecfc328b2f450aac858a59e24c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
