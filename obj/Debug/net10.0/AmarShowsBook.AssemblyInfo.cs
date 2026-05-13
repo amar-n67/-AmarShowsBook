fa70705adebe1ac2062c0352cd71eba9d3030473
Amar <amarnath4172@gmail.com>
2026-05-14 00:39:51 +0530

    Checkpoint admin management system with user analytics and transaction monitoring

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3cc0b428b06a8987faec852cbcfdf5379a56c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efec74d7237136fb185f00d57451ac320dad27ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
