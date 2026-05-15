ab20360ccf433876b20f1eafac82d83260cd7e45
Amar <amarnath4172@gmail.com>
2026-05-15 23:03:01 +0530

    Add responsive horizontal user card layout and fix admin access page styling

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a677745017379c80e9d4bfb0277fd6371d71ca0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b031529f4fe503c21eb6072a9b0a9965090179")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
