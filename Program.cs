ab3a26452dfdfe6471fef2f9d1c6e029aacd9303
Amar <115856901+amar-n67@users.noreply.github.com>
2026-06-02 12:19:47 +0530

    Enable automatic EF migrations on startup

--- Program.cs
+++ Program.cs
@@ -283,5 +283,11 @@ ex,
 }
 
 });
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    db.Database.Migrate();
+}
 
 app.Run();
--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46a501898eead85c5f3c9aa880de8f6b9f0567b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd15003ecafd9ed7e8f9145a19ba965f10b1f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
