15f6631b3e163f26f2a86d0331506ebd11faac97
Amar <115856901+amar-n67@users.noreply.github.com>
2026-06-27 19:39:29 +0530

    New breakpoint

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee03db2eaeb77ccb59d2090ec2f0fcfc939eccc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c73376a0fc24d4a06ba1177998e4d99f122ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
