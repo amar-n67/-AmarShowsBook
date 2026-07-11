46867dbe539c8dfb6b3607fc4ed63e7c013abaa0
Amar <115856901+amar-n67@users.noreply.github.com>
2026-07-11 15:48:57 +0530

    Remove EF migrations on startup

--- Program.cs
+++ Program.cs
@@ -70,12 +70,7 @@ connectionString
 
 var app=
 builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-    db.Database.Migrate();
-}
 var forwardedHeadersOptions =
 new ForwardedHeadersOptions
 {
