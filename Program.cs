57f95fb74b824bc81aae755649c28dfdec213de3
Amar <115856901+amar-n67@users.noreply.github.com>
2026-07-11 12:12:08 +0530

    Apply EF migrations on startup

--- Program.cs
+++ Program.cs
@@ -70,7 +70,12 @@ connectionString
 
 var app=
 builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
+    db.Database.Migrate();
+}
 var forwardedHeadersOptions =
 new ForwardedHeadersOptions
 {
