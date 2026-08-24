61cb9196ed02649e26691680b1fc4082b2db0134
Amar <115856901+amar-n67@users.noreply.github.com>
2026-08-24 13:22:56 +0530

    Fix admin security page layout and overlapping controls

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2af47a0e470fe28c9a80c1828af239de0d74510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fbcac79fbd39d9003a16a90b647401b86b41a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
