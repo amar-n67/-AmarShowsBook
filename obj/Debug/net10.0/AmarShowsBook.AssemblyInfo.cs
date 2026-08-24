ebd4e0f82ef6ba6dd7f1b81028310a24407767cb
Amar <115856901+amar-n67@users.noreply.github.com>
2026-08-24 09:26:22 +0530

    Improve home location and show type layout

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed4fe266cb8f39a0b8e328be1aba1de686192c53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282aff7cbff4972e9d3e1bb543f444c5d75ddf26")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
