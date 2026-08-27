d0603115cccc67967850507045e4cc7475883fa0
Amar <115856901+amar-n67@users.noreply.github.com>
2026-08-27 16:21:18 +0530

    Add cinematic animations to ShowTime hero

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c539d16db714d5b919fc9a6ab8dab76624bb36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699fb4763be77600037175c7bbdc39372b40be6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
