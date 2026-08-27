c3a4ab5ee09a8ab30cd597deb53b01255ea6951e
Amar <115856901+amar-n67@users.noreply.github.com>
2026-08-27 14:45:50 +0530

    Refine ShowTime current location nearby search and active filters

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3dad462ee7757b021563057bbb07a1a315cd12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee4b391ce9e26bf5634c18dd804e2c0b75a6145")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
