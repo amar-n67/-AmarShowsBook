43c539d16db714d5b919fc9a6ab8dab76624bb36
Amar <115856901+amar-n67@users.noreply.github.com>
2026-08-27 16:02:35 +0530

    Fix ShowTime show type filtering with selected location

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e6ae9c31383eedbee966d4e4637b2054fa7a26e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2546265e0d4353a98fe9102ee965961f567deaaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
