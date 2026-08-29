eb1ec45f51fdaeedc3f192f8d48fd0d55c105c78
Amar <115856901+amar-n67@users.noreply.github.com>
2026-08-29 10:52:30 +0530

    Match News page buttons with Developer Profile styling

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ab0581e1462a7f4006d3940321e2cc21e591dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8a66b6290237e9a42edbefca50f2c5b76a7db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
