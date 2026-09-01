834f48457a6a123da213515790bf0b068a50c22f
Amar <115856901+amar-n67@users.noreply.github.com>
2026-09-01 18:45:27 +0530

    Add temporary theme preview on hover

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6853f2be011f1dc074c46f1511fe83efd4682312")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba588ccca3af7615332d073a261eb136c3edd1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
