129592983628737d26b96c17c6dd07c958d687fc
Amar <115856901+amar-n67@users.noreply.github.com>
2026-09-01 18:24:02 +0530

    Standardize contact icons and fix duplicate Back to Home icon

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a0cfff268839db5dd2f80d4a21063334aba123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d6eadd4e22711eaeb2457876b5872f37cd3b256")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
