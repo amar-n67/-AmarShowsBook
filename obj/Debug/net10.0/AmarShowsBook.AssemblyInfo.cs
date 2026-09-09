158ba690e13df6d9db06e66f11c27eec990103be
Amar <115856901+amar-n67@users.noreply.github.com>
2026-09-10 02:12:34 +0530

    Add btns.

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28c01613d0cb1cdae3c90059be8570a51b9be7c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30d6f73f2cdd6a5b961455094995b4a784c7539")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
