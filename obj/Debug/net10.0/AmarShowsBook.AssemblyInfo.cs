986e02f3e0f874a0aa55373c70fb8cba3b6dd40b
Amar <115856901+amar-n67@users.noreply.github.com>
2026-09-10 21:31:44 +0530

    Fixes bugs and Redesign login page.

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9556e537d86fdd6811107daed877a0ad0e929d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f83ac0c8fcfa710c16ccaa8c4e05ef311d813fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
