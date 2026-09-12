4b5a30ad719f4c81ec74672b8d018ddcbe6040f4
Amar <115856901+amar-n67@users.noreply.github.com>
2026-09-12 22:54:38 +0530

    Bug fixes.

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64cd5318fbf95550351ef3c4e00bf5b6b5e46a1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87df96ef7315df9a385fac686165917b3a62f5d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
