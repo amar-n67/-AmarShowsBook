c3582541e73bed775f4e7269fbaadfe98fcf5389
Amar <115856901+amar-n67@users.noreply.github.com>
2026-09-12 22:16:07 +0530

    Fixes bug  Home page.

--- obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
+++ obj/Debug/net10.0/AmarShowsBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7304f50ef10814964e25cd73799364d07633ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d7d6632c2996f72919cbfb3ffea515100e3ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmarShowsBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
